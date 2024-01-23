20468c2c7c2b369cfc98903a93d5a8fe6478976e
Andreas Jensen <Andr4376@gmail.com>
2024-01-23 11:27:07 +0100

    custom player transform

--- Assets/src/Scripts/Network/PlayerTransform.cs
+++ Assets/src/Scripts/Network/PlayerTransform.cs
@@ -1,107 +1,48 @@
-using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
 
-/// <summary>
-/// An example network serializer with both server and owner authority.
-/// Love Tarodev
-/// </summary>
 public class PlayerTransform : NetworkBehaviour
 {
-    /// <summary>
-    /// A toggle to test the difference between owner and server auth.
-    /// </summary>
-    [SerializeField] private bool _serverAuth;
-    [SerializeField] private float _cheapInterpolationTime = 0.1f;
+    public float lerpSpeed = 10f;
 
-    private NetworkVariable<PlayerNetworkState> _playerState;
-    private Rigidbody _rb;
+    // Define a struct to hold position and rotation data
+    public struct TransformData : INetworkSerializable
+    {
+        public Vector3 Position;
+        public float RotationY;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref Position);
+            serializer.SerializeValue(ref RotationY);
+        }
+    }
+
+    // Create a NetworkVariable to hold the transform data
+    private NetworkVariable<TransformData> networkTransform;
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-
-        var permission = _serverAuth ? NetworkVariableWritePermission.Server : NetworkVariableWritePermission.Owner;
-        _playerState = new NetworkVariable<PlayerNetworkState>(writePerm: permission);
-    }
-
-    public override void OnNetworkSpawn()
-    {
-        if (!IsOwner) Destroy(transform.GetComponent<PlayerController>());
+        var permission = NetworkVariableWritePermission.Owner;
+        networkTransform = new NetworkVariable<TransformData>(writePerm: permission);
     }
 
     private void Update()
     {
-        if (IsOwner) TransmitState();
-        else ConsumeState();
-    }
-
-    #region Transmit State
-
-    private void TransmitState()
-    {
-        var state = new PlayerNetworkState
+        if (IsOwner)
         {
-            Position = _rb.position,
-            Rotation = transform.rotation.eulerAngles
-        };
-
-        if (IsServer || !_serverAuth)
-            _playerState.Value = state;
-        else
-            TransmitStateServerRpc(state);
-    }
-
-    [ServerRpc]
-    private void TransmitStateServerRpc(PlayerNetworkState state)
-    {
-        _playerState.Value = state;
-    }
-
-    #endregion
-
-    #region Interpolate State
-
-    private Vector3 _posVel;
-    private float _rotVelY;
-
-    private void ConsumeState()
-    {
-        // Here you'll find the cheapest, dirtiest interpolation you'll ever come across. Please do better in your game
-        _rb.MovePosition(Vector3.SmoothDamp(_rb.position, _playerState.Value.Position, ref _posVel, _cheapInterpolationTime));
-
-        transform.rotation = Quaternion.Euler(
-            0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _playerState.Value.Rotation.y, ref _rotVelY, _cheapInterpolationTime), 0);
-    }
-
-    #endregion
-
-    private struct PlayerNetworkState : INetworkSerializable
-    {
-        private float _posX, _posZ;
-        private short _rotY;
-
-        internal Vector3 Position
-        {
-            get => new(_posX, 0, _posZ);
-            set
+            // If this is the owner, update the NetworkVariable with the current transform
+            networkTransform.Value = new TransformData
             {
-                _posX = value.x;
-                _posZ = value.z;
-            }
+                Position = transform.position,
+                RotationY = transform.rotation.eulerAngles.y
+            };
         }
-
-        internal Vector3 Rotation
+        else
         {
-            get => new(0, _rotY, 0);
-            set => _rotY = (short)value.y;
-        }
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
-        {
-            serializer.SerializeValue(ref _posX);
-            serializer.SerializeValue(ref _posZ);
-
-            serializer.SerializeValue(ref _rotY);
+            // If this is not the owner, interpolate the transform with the NetworkVariable
+            transform.position = Vector3.Lerp(transform.position, networkTransform.Value.Position, Time.deltaTime * lerpSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, networkTransform.Value.RotationY, 0), Time.deltaTime * lerpSpeed);
         }
     }
 }
